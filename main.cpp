a45c089c838e36eb1fd426620724fb1921883c7f
Alex <alexguz8279@gmail.com>
2024-05-04 22:13:06 +0500

    Added remove methods. Generally linked list already works

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ template <class T>
 class ListNode {
 public:
     T data_;
-    ListNode* next;
-    ListNode* prev;
+    ListNode<T>* next;
+    ListNode<T>* prev;
 
     explicit ListNode() = default;
     explicit ListNode(T data) : data_{ data } {}
@@ -14,8 +14,10 @@ public:
 template <class T>
 class List {
 public:
+    List() : head{ nullptr }, tail{ nullptr } {}
+
     void AddStart(T n) {
-        ListNode* node = new ListNode{ n };
+        ListNode<T>* node = new ListNode{ n };
 
         if (head == nullptr) tail = node;
         else {
@@ -27,7 +29,7 @@ public:
     }
 
     void AddEnd(T n) {
-        ListNode* node = new ListNode{ n };
+        ListNode<T>* node = new ListNode{ n };
 
         if (head == nullptr) {
             head = node;
@@ -41,44 +43,72 @@ public:
     }
 
     void InsertAfter(T after_n, T n) {
-        ListNode* search_node = LinearSearch_(after_n);
+        ListNode<T>* search_node = LinearSearch_(after_n);
 
         if (search_node) {
-            ListNode* node = new ListNode{ n };
+            ListNode<T>* node = new ListNode{ n };
 
             if (search_node == tail) tail = node;
             if (search_node->next) search_node->next->prev = node;
             node->next = search_node->next;
             search_node->next = node;
-            node.prev = search_node;
+            node->prev = search_node;
         }
     }
 
     // Need to use smart pointers
-    void PopFront(T n) {
+    void PopFront() {
         if (head == nullptr) return;
 
         head->next->prev = nullptr;
-        head = head.next;
+        head = head->next;
     }
 
-    void PopBack(T n) {
+    void PopBack() {
         if (head == nullptr) return;
 
         tail->prev->next = nullptr;
         tail = tail->prev;
     }
 
+    void Remove(T n) {
+        ListNode<T>* search_node = LinearSearch_(n);
+
+        if (search_node == tail) return;
+
+        if (search_node == head) {
+            search_node->next->prev = nullptr;
+            head = search_node->next;
+        }
+
+        if (search_node) {
+            search_node->prev->next = search_node->next;
+            search_node->next->prev = search_node->prev;
+        }
+    }
+
+    void Print() const {
+        if (head == nullptr) return;
+
+        ListNode<T>* current = head;
+
+        while (current) {
+            std::cout << current->data_ << ' ';
+            current = current->next;
+        }
+        std::cout << '\n';
+    }
+
 private:
-    ListNode* head;
-    ListNode* tail;
+    ListNode<T>* head;
+    ListNode<T>* tail;
 
     // Trouble with strings
-    ListNode* LinearSearch_(T n) {
-        ListNode* search_node = head;
+    ListNode<T>* LinearSearch_(T n) {
+        ListNode<T>* search_node = head;
 
         while (search_node != nullptr) {
-            if (search_node->data == n) return search_node;
+            if (search_node->data_ == n) return search_node;
             search_node = search_node->next;
         }
         return nullptr;
@@ -86,5 +116,17 @@ private:
 };
 
 int main() {
+    List<int> list1;
 
+    list1.AddStart(1);
+    list1.AddStart(2);
+    list1.AddStart(3);
+    list1.AddStart(4);
+    list1.AddEnd(10);
+    
+    list1.Print();
+
+    list1.Remove(555);
+
+    list1.Print();
 }
